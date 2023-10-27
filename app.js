cda7a937ca49d89e257b31e47e26921c63c91dc7
MajaNik86 <majanik86@gmail.com>
2023-10-27 11:18:37 +0200

    added signup ,login ejs,styles for form, routes and controller for rendering signup and login

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 
 const db= require('./data/database')
 const baseRoutes = require('./router/base.router');
-
+const authRoutes = require('./router/auth.router')
 
 const app = express();
 
@@ -12,6 +12,7 @@ app.set('views',path.join(__dirname,'views'));
 app.use(express.static('public')) 
 
 app.use(baseRoutes);
+app.use(authRoutes)
 
 db.connectToDb().then(function(){
     app.listen(3000)
