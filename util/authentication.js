e590a483d1b1c87e4688bb50e66595f24715eb06
MajaNik86 <majanik86@gmail.com>
2023-11-03 11:16:56 +0100

    added isAdmin authorizaition and customized navbar according to isAuth and isAdmin

--- util/authentication.js
+++ util/authentication.js
@@ -2,7 +2,8 @@
 
 function createUserSession(req, user, action) {
     req.session.uid = user._id.toString();
-    req.session.save(action) ;
+    req.session.isAdmin = user.isAdmin;
+req.session.save(action) ;
 }
 
 function destroyUserAuthSession(req) {
