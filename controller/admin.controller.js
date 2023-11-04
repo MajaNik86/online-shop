a3b75e8ec55819726de8d87e0482c16d6e4ccb66
MajaNik86 <majanik86@gmail.com>
2023-11-04 11:24:23 +0100

    added logic to update product, admin controller function, model

--- controller/admin.controller.js
+++ controller/admin.controller.js
@@ -1,15 +1,14 @@
 const Product = require("../model/product.model");
 
-
 async function getProducts(req, res, next) {
-    try {
-      const products = await Product.findAll();
-      res.render('admin/products/all-products', { products: products });
-    } catch (error) {
-      next(error);
-      return;
-    }
+  try {
+    const products = await Product.findAll();
+    res.render("admin/products/all-products", { products: products });
+  } catch (error) {
+    next(error);
+    return;
   }
+}
 
 function getNewProduct(req, res) {
   res.render("admin/products/new-product");
@@ -30,24 +29,38 @@ async function createNewProduct(req, res, next) {
 
   res.redirect("/admin/products");
 }
-async function getUpdateProduct(req,res,next){
-  try{
+async function getUpdateProduct(req, res, next) {
+  try {
     const product = await Product.findById(req.params.id);
-    res.render('admin/products/update-product',{product:product})
+    res.render("admin/products/update-product", { product: product });
+  } catch (error) {
+    next(error);
+  }
+}
+async function updateProduct(req, res, next) {
+  const product = new Product({
+    ...req.body,
+    _id: req.params.id,
+  });
 
-  }catch(error){
-    next(error)
+  if (req.file) {
+    product.replaceImage(req.file.filename);
   }
 
-}
-function updateProduct(){
-
+  try {
+    await product.save();
+  } catch (error) {
+    next(error);
+    return;
+  }
+  
+  res.redirect('/admin/products');
 }
 
 module.exports = {
   getProducts: getProducts,
   getNewProduct: getNewProduct,
   createNewProduct: createNewProduct,
-  getUpdateProduct:getUpdateProduct,
-  updateProduct:updateProduct
+  getUpdateProduct: getUpdateProduct,
+  updateProduct: updateProduct,
 };
