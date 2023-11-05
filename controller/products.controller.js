6c16fd63ac1b5004cf1c689399fd1010e69e68fa
MajaNik86 <majanik86@gmail.com>
2023-11-05 15:06:30 +0100

    added product detail page

--- controller/products.controller.js
+++ controller/products.controller.js
@@ -1,14 +1,21 @@
-const Product = require('../model/product.model')
+const Product = require("../model/product.model");
 
- async function getAllProducts(req,res,error){
-    try{
-       const products = await Product.findAll()
-       res.render('customer/products/all-products',{products:products})
-
-    } catch(error){
-
-    next(error)
-    }
+async function getAllProducts(req, res, error) {
+  try {
+    const products = await Product.findAll();
+    res.render("customer/products/all-products", { products: products });
+  } catch (error) {
+    next(error);
+  }
 }
 
-module.exports = {getAllProducts:getAllProducts}
+async function getProductDetails(req, res, next) {
+   try {
+     const product = await Product.findById(req.params.id);
+       res.render('customer/products/product-details',{product:product})
+   } catch (error) {
+     return next(error);
+   }
+ }
+
+module.exports = { getAllProducts: getAllProducts , getProductDetails:getProductDetails};
--- router/products.router.js
+++ router/products.router.js
@@ -2,7 +2,9 @@ const express = require('express');
 const productsController = require('../controller/products.controller')
 const router = express.Router();
 
-router.get('/products',productsController.getAllProducts)
+router.get('/products',productsController.getAllProducts);
+
+router.get('/products/:id',productsController.getProductDetails);
 
 
 module.exports = router 
