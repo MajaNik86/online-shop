83851a4cb0763cee1df43b6a66eb45a0597b4a2a
MajaNik86 <majanik86@gmail.com>
2023-11-06 12:06:33 +0100

    backend code for updating shopping cart, added new function in controller, method in model and route

--- router/cart.router.js
+++ router/cart.router.js
@@ -5,6 +5,7 @@ const router = express.Router();
 
 router.get('/',cartController.getCart);
 router.post('/items',cartController.addCartItem )
+router.patch('/items',cartController.updateCartItem);
 
 
 module.exports=router;
